68c16ff868839414c3f5f722ad3e59dd262827cd
Chris Casola <ccasola@wpi.edu>
2012-10-28 17:31:06 -0400

    made fields of Move final

---
+++ ConnectN/src/edu/wpi/cs/connectn/tree/Node.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs.connectn.tree;
+
+public class Node {
+	protected Node[] children;
+	protected Move move;
+	protected int utility;
+	protected Node parent;
+	
+	public Node(int col)
+}
