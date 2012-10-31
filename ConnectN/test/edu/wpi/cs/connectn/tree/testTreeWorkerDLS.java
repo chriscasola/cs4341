508671292e97bde014e8b2db93898f933523489c
Chris Casola <ccasola@wpi.edu>
2012-10-31 01:13:03 -0400

    Added test case for treeWorkerDLS

--- ConnectN/test/edu/wpi/cs/connectn/tree/testTreeWorkerDLS.java
+++ ConnectN/test/edu/wpi/cs/connectn/tree/testTreeWorkerDLS.java
@@ -5,15 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.wpi.cs.connectn.board.JournaledBoard;
+import edu.wpi.cs.connectn.board.Minimax;
+
 public class testTreeWorkerDLS {
+	
+	protected TreeWorkerDLS treeWorker;
+	protected JournaledBoard jboard;
+	protected Node startNode;
+	
 
 	@Before
 	public void setUp() throws Exception {
+		Move firstMove = new Move(1, Minimax.MAX);
+		jboard = new JournaledBoard(3, 3, 2);
+		startNode = new Node(firstMove, null);
+		treeWorker = new TreeWorkerDLS(jboard, startNode, new BasicHeuristicFunction(), new BasicUtilityFunction());
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		treeWorker.buildTree(startNode, 1);
+		treeWorker.buildTree(startNode, 2);
+		treeWorker.buildTree(startNode, 3);
+		System.out.println(startNode.toString());
+		assertTrue(true);
 	}
 
 }
