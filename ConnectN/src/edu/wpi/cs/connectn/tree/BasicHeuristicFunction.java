fec5442c2ab131cb518fc0fa144e9d45b43234e2
JPage <chrispage@wpi.edu>
2012-10-31 13:14:58 -0400

    Modified comments in BasicHeuristicFunction

--- ConnectN/src/edu/wpi/cs/connectn/tree/BasicHeuristicFunction.java
+++ ConnectN/src/edu/wpi/cs/connectn/tree/BasicHeuristicFunction.java
@@ -3,15 +3,15 @@ package edu.wpi.cs.connectn.tree;
 import edu.wpi.cs.connectn.board.Board;
 
 /**
- * A basic heuristic function 
- * @author Chris
- *
+ * A basic heuristic function.
  */
 public class BasicHeuristicFunction implements HeuristicFunction {
 
 	/**
 	 * Estimates the utility of the given node based on the column number and the depth of the node
 	 * NOTE: This does not perform well, a very stupid algorithm
+	 * 
+	 * @see HeuristicFunction#calcHeuristic(Board, Node)
 	 */
 	@Override
 	public float calcHeuristic(Board board, Node node) {
