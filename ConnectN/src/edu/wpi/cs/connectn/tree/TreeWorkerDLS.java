698aa2117ab2ea3c94f566540a366e4561a5f0d4
Chris Casola <ccasola@wpi.edu>
2012-10-30 20:13:28 -0400

    Added shell classes for TreeWorkerDLS and TreeWorkerIDDFS.

---
+++ ConnectN/src/edu/wpi/cs/connectn/tree/TreeWorkerDLS.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs.connectn.tree;
+
+public class TreeWorkerDLS extends Thread {
+
+	@Override
+	public void run() {
+		
+	}
+	
+	protected Node buildTree(Node startNode, int depth) {
+		return startNode;
+	}
+}
---
+++ ConnectN/src/edu/wpi/cs/connectn/tree/TreeWorkerIDDFS.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs.connectn.tree;
+
+public class TreeWorkerIDDFS extends TreeWorkerDLS {
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	protected Node buildTree(Node startNode) {
+		return startNode; // TODO
+	}
+}
