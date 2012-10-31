f0f70f7976d5949a5274c854f186493a0bdd19d7
JPage <chrispage@wpi.edu>
2012-10-31 19:58:43 -0400

    committed for Chris

--- ConnectN/src/edu/wpi/cs/connectn/Player.java
+++ ConnectN/src/edu/wpi/cs/connectn/Player.java
@@ -1,51 +1,96 @@
 package edu.wpi.cs.connectn;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
+import edu.wpi.cs.connectn.board.Minimax;
+import edu.wpi.cs.connectn.tree.Move;
+import edu.wpi.cs.connectn.tree.Node;
+import edu.wpi.cs.connectn.tree.SearchManager;
+
+/**
+ * @author Christopher Casola
+ * @author Jennifer Page
+ *
+ */
 public class Player {
+	/** The name of the Player. */
+	private static final String playerName = "the-masked-unit";
+	
+	/** The height of the board as defined by the referee. */
+	private static int boardHeight;
+	
+	/** The width of the board as defined by the referee. */
+	private static int boardWidth;
+	
+	/** The number of pieces that have to be connected to win. */
+	private static int numToWin;
+	
+	/** Whether or not the player is going first. */
+	private static boolean firstPlayer;
+	
+	/** The time limit in seconds for making a move. */
+	private static int timeLimit;
 
-	public static void main(String[] args) {
-		int width, height, numToWin, playerNumber, timeLimit, move;
+	/**
+	 * Main method. Communicates with agent and commands SearchManager.
+	 * 
+	 * @param args
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		SearchManager sm;
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-		// use BufferedReader for easy reading
-		BufferedReader input = new BufferedReader(
-				new InputStreamReader(System.in));
-
-		// send player name
-		System.out.println("the-masked-unit");
+		// Send Player name to referee
+		System.out.println(playerName);
 		System.out.flush();
 
-		// read game config
-		String [] gameConfig = input.readLine().split(" ");
-		height = Integer.parseInt(gameConfig[0]);
-		width = Integer.parseInt(gameConfig[1]);
+		// Read the game information provided by the referee.
+		String[] gameConfig = input.readLine().split(" ");
+		boardHeight = Integer.parseInt(gameConfig[0]);
+		boardWidth = Integer.parseInt(gameConfig[1]);
 		numToWin = Integer.parseInt(gameConfig[2]);
-		playerNumber = Integer.parseInt(gameConfig[3]);
+		firstPlayer = (Integer.parseInt(gameConfig[3]) == 0) ? true : false;
 		timeLimit = Integer.parseInt(gameConfig[4]);
+		
+		// If the Player is first, construct the SearchManager with the optimal first Move and tell it to 
+		// search before printing the column of the firstMove
+		if (firstPlayer) {
+			Move firstMove = new Move(boardWidth/2, Minimax.MAX);
+			sm = new SearchManager(boardHeight, boardWidth, numToWin, firstMove);
+			sm.searchToDepth(5);
+			System.out.println(firstMove.getColumn());
+		}
+		// If the Player is second, get the opponent's first move, construct the SearchManager and tell it to
+		// search before printing the best move as determined by the Searchmanager
+		else {
+			Move firstMove = new Move(Integer.parseInt(input.readLine()), Minimax.MIN);
+			sm = new SearchManager(boardHeight, boardWidth, numToWin, firstMove);
+			sm.searchToDepth(5);
+			Move myBestMove = sm.getBestMove();
+			sm.makeMove(myBestMove);
+			System.out.println(myBestMove.getColumn());
+		}
 
-		int currentTurn = 0;	// first player starts
-		while (true) {
-			if (currentTurn == playerNumber) {
-				// TODO: use a mechanism for timeout(threads, java.util.Timer, ..)
-
-				// call alpha-beta algorithm to get the move
-				move = getMove();
-
-				// send move
-				System.out.println(String.valueOf(move));
-				System.out.flush();
-			} else {
-				// read move
-				move = Integer.parseInt(input.readLine());
-
-				// check for end
-				if (move < 0)
-					break;
+		
+		// Get the opponent's move, do some searching, and then end.
+		int opponentMove;
+		while(true) {
+			opponentMove = Integer.parseInt(input.readLine());
+			
+			if (opponentMove < 0) {
+				break;
 			}
-
-			// switch turns
-			currentTurn = 1 - currentTurn;
+			
+			sm.makeMove(new Move(opponentMove, Minimax.MIN));
+			sm.searchToDepth(5);
+			Move myMove = sm.getBestMove();
+			sm.makeMove(myMove);
+			System.out.println(myMove.getColumn());
 		}
 	}
 }
--- ConnectN/src/edu/wpi/cs/connectn/tree/SearchManager.java
+++ ConnectN/src/edu/wpi/cs/connectn/tree/SearchManager.java
@@ -6,18 +6,18 @@ import edu.wpi.cs.connectn.board.JournaledBoard;
  * Manages searching.
  */
 public class SearchManager {
-	
+
 	/** The JournaledBoard representing past moves. */
 	protected final JournaledBoard jboard;
-	
+
 	/** The Tree containing potential Moves to make. */
 	protected final Tree tree;
-	
+
 	protected int timeToMove;
-	
+
 	/** The current depth that the graph has been searched. */
 	protected int currentDepth;
-	
+
 	/**
 	 * Constructs a SearchManager.
 	 * 
@@ -29,39 +29,41 @@ public class SearchManager {
 	 * @param N
 	 * @param firstMove
 	 */
-	protected SearchManager(int timeToMove, int height, int width, int N, Move firstMove) {
+	/*protected SearchManager(int timeToMove, int height, int width, int N, Move firstMove) {
 		this.timeToMove = timeToMove;
 		this.tree = new Tree(new Node(firstMove, null));
 		this.jboard = new JournaledBoard(height, width, N);
 		this.currentDepth = 0;
+	}*/
+
+	public SearchManager(int boardHeight, int boardWidth, int numToWin, Move firstMove) {
+		this.tree = new Tree(new Node(firstMove, null));
+		this.jboard = new JournaledBoard(boardHeight, boardWidth, numToWin);
+		this.currentDepth = 0;
 	}
-	
+
 	/**
 	 * Make a Move in the given column number.
 	 * 
-	 * @param colNum	An integer representing a column number.
+	 * @param move	The Move to make.
 	 */
-	public void makeMove(int colNum) {
-		tree.makeMove(colNum);
+	public void makeMove(Move move) {
+		jboard.doMove(move);
+		tree.makeMove(move.getColumn());
+		currentDepth--;
 	}
-	
-	/**
-	 * Finds, performs in the Tree, and returns the optimal Move.
-	 * 
-	 * @return	An integer representing the column in which the optimal Move takes place.
-	 */
-	public int findMaxMove() {
-		
-		// should check for time elapsed
-		int count = 10;
-		while (count-- > 0) {
-			TreeWorkerIDDFS mainWorker = new TreeWorkerIDDFS(jboard.duplicate(), tree.getHead(), currentDepth + 5, currentDepth, new BasicHeuristicFunction(), new BasicUtilityFunction());
+
+	public void searchToDepth(int depth) {
+		if (depth > currentDepth) {
+			TreeWorkerIDDFS mainWorker = new TreeWorkerIDDFS(jboard.duplicate(), tree.getHead(), depth, currentDepth, new BasicHeuristicFunction(), new BasicUtilityFunction());
 			mainWorker.run();
-			currentDepth += 5;
+			currentDepth = depth;
 		}
-		
+	}
+
+	public Move getBestMove() {
 		Node bestChild = null;
-		
+
 		for (Node child : tree.getHead().getChildren()) {
 			if (bestChild == null) {
 				bestChild = child;
@@ -72,10 +74,41 @@ public class SearchManager {
 				}
 			}
 		}
-		
+
+		return bestChild.getMove();
+	}
+
+	/**
+	 * Finds, performs in the Tree, and returns the optimal Move.
+	 * 
+	 * @return	An integer representing the column in which the optimal Move takes place.
+	 */
+	public int findMaxMove() {
+
+		// should check for time elapsed
+		int count = 10;
+		while (count-- > 0) {
+			TreeWorkerIDDFS mainWorker = new TreeWorkerIDDFS(jboard.duplicate(), tree.getHead(), currentDepth + 5, currentDepth, new BasicHeuristicFunction(), new BasicUtilityFunction());
+			mainWorker.run();
+			currentDepth += 5;
+		}
+
+		Node bestChild = null;
+
+		for (Node child : tree.getHead().getChildren()) {
+			if (bestChild == null) {
+				bestChild = child;
+			}
+			else {
+				if (bestChild.getUtility() < child.getUtility()) {
+					bestChild = child;
+				}
+			}
+		}
+
 		tree.makeMove(bestChild.getMove());
 		currentDepth--;
-		
+
 		return bestChild.getMove().getColumn();
 	}
 }
