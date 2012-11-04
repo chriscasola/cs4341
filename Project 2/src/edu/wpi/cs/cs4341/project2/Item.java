cc99b13c5a995a6863a3eac5ee81328acf079fd5
JPage <chrispage@wpi.edu>
2012-11-03 20:49:26 -0400

    initial commit

---
+++ Project 2/src/edu/wpi/cs/cs4341/project2/Item.java
@@ -0,0 +1,114 @@
+package edu.wpi.cs.cs4341.project2;
+
+/**
+ * Represents an Item.
+ */
+public class Item {
+	
+	/** The uppercase letter representing the item. */
+	protected final char id;
+	
+	/** The weight of the item. */
+	protected final int weight;
+	
+	/**
+	 * Constructs an Item.
+	 * 
+	 * @param id		An uppercase letter that represents the Item.
+	 * @param weight	The Item's weight.
+	 */
+	public Item(char id, int weight) {
+		if (id < 65 || id > 90) {
+			throw new RuntimeException("The first parameter contained in the given String must be an uppercase letter.");
+		}
+		
+		this.id = id;
+		this.weight = weight;
+	}
+	
+	/**
+	 * Getter for the Item's id.
+	 * 
+	 * @return A char representing the Item's id.
+	 */
+	public char getId() {
+		return id;
+	}
+	
+	/**
+	 * Getter for the Item's weight.
+	 * 
+	 * @return An int representing the Item's weight.
+	 */
+	public int getWeight() {
+		return weight;
+	}
+	
+	/**
+	 * Determines whether or not an instance of an Item is equal to a given Object.
+	 * 
+	 * @param object	The Object to compare the Item to.
+	 * 
+	 * @return	True if the given Object is equal to the Item. False otherwise.
+	 */
+	@Override
+	public boolean equals(Object object) {
+		// compare instances
+		if (this == object) {
+			return true;
+		}
+		
+		// if the Object is and instance of Item
+		if (object instanceof Item) {
+			Item item = (Item) object;
+			
+			// check that the values of the toString function are equal
+			if (this.toString().equals(item.toString())) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+				
+	}
+	
+	/**
+	 * Returns a String representation of the Item instance.
+	 * 
+	 * @return A String representation of the Item instance.
+	 */
+	@Override
+	public String toString() {
+		return id + " " + weight;
+	}
+	
+	/**
+	 * Returns a new Item created from the given String.
+	 * 
+	 * @param itemString	The String to build an Item from.
+	 * 
+	 * @return	An Item created using .
+	 */
+	public static Item fromString(String itemString) {
+		String[] parameters = itemString.split(" ");
+		
+		// Ensure there are only two parameters in the String
+		if (parameters.length != 2) {
+			throw new RuntimeException("The given String contains too many parameters.");
+		}
+		// Ensure the first parameter is 1 character long
+		else if (parameters[0].length() != 1) {
+			throw new RuntimeException("The first parameter contained in the given String must only be one character long.");
+		}
+		else {
+			char id = parameters[0].charAt(0);
+			int weight = Integer.parseInt(parameters[1]);
+			
+			return new Item(id, weight);
+		}
+	}
+}
---
+++ Project 2/src/edu/wpi/cs/cs4341/project2/constraints/Constraint.java
@@ -0,0 +1,8 @@
+package edu.wpi.cs.cs4341.project2.constraints;
+
+/**
+ * An abstract class representation of a constraint.
+ */
+public abstract class Constraint {
+	// TODO
+}
