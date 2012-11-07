50582710919226cdc62bf0b36094cf2205cd4527
JPage <chrispage@wpi.edu>
2012-11-06 22:25:31 -0500

    EqualBinaryConstraint mostly done and satisfied method is tested

--- Project 2/src/edu/wpi/cs/cs4341/project2/constraints/EqualBinaryConstraint.java
+++ Project 2/src/edu/wpi/cs/cs4341/project2/constraints/EqualBinaryConstraint.java
@@ -2,18 +2,34 @@ package edu.wpi.cs.cs4341.project2.constraints;
 
 import edu.wpi.cs.cs4341.project2.Item;
 
-// TODO
+/**
+ * Represents an equal binary constraint. This constraint requires that two specific Items are not assigned to the same Bag.
+ */
 public class EqualBinaryConstraint extends BinaryConstraint {
-
+	/**
+	 * Constructs an EqualBinaryConstraint.
+	 * 
+	 * @param item1		One item involved with this constraint.
+	 * @param item2		Another item involved with this constraint.
+	 */
 	public EqualBinaryConstraint(Item item1, Item item2) {
 		super(item1, item2);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Satisfaction satisfied() {
-		// TODO Auto-generated method stub
-		return null;
+		if (item1.getAssignedBag() == null && item2.getAssignedBag() == null) {
+			return Satisfaction.NONE;
+		}
+		else if (item1.getAssignedBag() == null ^ item2.getAssignedBag() == null) {
+			return Satisfaction.PARTIAL;
+		}
+		else if (item1.getAssignedBag().equals(item2.getAssignedBag())) {
+			return Satisfaction.COMPLETE;
+		}
+		else {
+			return Satisfaction.BROKEN;
+		}
 	}
 
 }
