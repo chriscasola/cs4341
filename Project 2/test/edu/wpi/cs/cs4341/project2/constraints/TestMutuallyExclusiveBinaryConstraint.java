23f69717ee2181f9b05cf9499d04b9bab31e2a23
JPage <chrispage@wpi.edu>
2012-11-08 18:25:57 -0500

    started TestMutuallyExclusiveBinaryConstraint#fromString

--- Project 2/test/edu/wpi/cs/cs4341/project2/constraints/TestMutuallyExclusiveBinaryConstraint.java
+++ Project 2/test/edu/wpi/cs/cs4341/project2/constraints/TestMutuallyExclusiveBinaryConstraint.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs.cs4341.project2.constraints;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +24,23 @@ public class TestMutuallyExclusiveBinaryConstraint {
 		
 		item1 = new Item('A', 10);
 		item2 = new Item('B', 10);
+	}
+	
+	@Test
+	public void testConstructorExceptions() {
+		try {
+			MutuallyExclusiveBinaryConstraint constraint = new MutuallyExclusiveBinaryConstraint(item1, item2, null, bags[1]);
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The parameter bag1 must not be null."));
+		}
+		
+		try {
+			MutuallyExclusiveBinaryConstraint constraint = new MutuallyExclusiveBinaryConstraint(item1, item2, bags[0], null);
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The parameter bag2 must not be null."));
+		}
 	}
 
 	@Test
@@ -60,5 +78,19 @@ public class TestMutuallyExclusiveBinaryConstraint {
 		item2.setAssignedBag(null);
 		item2.setAssignedBag(bags[0]);
 		assertTrue(Satisfaction.BROKEN == mebConstraint.satisfied());
+	}
+	
+	@Test
+	public void testFromString() {
+		Item[] items = new Item[2];
+		items[0] = item1;
+		items[1] = item2;
+		
+		MutuallyExclusiveBinaryConstraint constraint = MutuallyExclusiveBinaryConstraint.fromString("A B a b", items, bags);
+		assertTrue(items[0].equals(constraint.item1));
+		assertTrue(items[1].equals(constraint.item2));
+		assertTrue(bags[0].equals(constraint.bags[0]));
+		assertTrue(bags[1].equals(constraint.bags[1]));
+		
 	}
 }
