ff26d2a86795bf383afab1eb144e77b639dd1ee6
JPage <chrispage@wpi.edu>
2012-11-08 14:48:59 -0500

    Updated RuntimeException message in CapacityConstraint#fromString. Finished TestCapacityConstraint tests.

--- Project 2/test/edu/wpi/cs/cs4341/project2/constraints/TestCapacityConstraint.java
+++ Project 2/test/edu/wpi/cs/cs4341/project2/constraints/TestCapacityConstraint.java
@@ -1,6 +1,9 @@
 package edu.wpi.cs.cs4341.project2.constraints;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +15,7 @@ import edu.wpi.cs.cs4341.project2.constraints.Constraint.Satisfaction;
 public class TestCapacityConstraint {
 	protected Bag bag;
 	protected Item[] items;
-	
+
 	@Before
 	public void setUp() {
 		bag = new Bag('a', 80);
@@ -22,16 +25,48 @@ public class TestCapacityConstraint {
 		items[2] = new Item('C', 10);
 		items[3] = new Item('D', 10);
 	}
-	
+
+	@Test
+	public void testConstructorForExceptions() {
+		CapacityConstraint constraint;
+
+		// test for minimum parameter greater than maximum parameter exception
+		try {
+			constraint = new CapacityConstraint(3, 1, bag, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The value of minimum may not be larger than the value of maximum."));
+		}
+
+		// test for bag parameter null exception
+		try {
+			constraint = new CapacityConstraint(1, 3, null, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The parameter bag must not be null."));
+		}
+
+		// test for items parameter null exception
+		try {
+			constraint = new CapacityConstraint(3, 3, bag, null);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The parameter items must not be null."));
+		}
+	}
+
 	@Test
 	public void testSatisfaction() {
 		CapacityConstraint capacityConstraint = new CapacityConstraint(2, 3, bag, items);
 
 		assertTrue(Satisfaction.NONE == capacityConstraint.satisfied());
-		
+
 		items[0].setAssignedBag(bag);
 		assertTrue(Satisfaction.PARTIAL == capacityConstraint.satisfied());
-		
+
 		items[1].setAssignedBag(bag);
 		assertTrue(Satisfaction.COMPLETE == capacityConstraint.satisfied());
 
@@ -40,5 +75,68 @@ public class TestCapacityConstraint {
 
 		items[3].setAssignedBag(bag);
 		assertTrue(Satisfaction.BROKEN == capacityConstraint.satisfied());
+	}
+	
+	@Test
+	public void testHasItem() {
+		CapacityConstraint constraint = new CapacityConstraint(1, 3, bag, items);
+		
+		assertTrue(constraint.hasItem(items[0]));
+		assertTrue(constraint.hasItem(items[1]));
+		assertTrue(constraint.hasItem(items[2]));
+		assertTrue(constraint.hasItem(items[3]));
+		
+		assertFalse(constraint.hasItem(new Item('Z', 10)));
+	}
+	
+	@Test
+	public void testHasBag() {
+		CapacityConstraint constraint = new CapacityConstraint(1, 3, bag, items);
+		
+		assertTrue(constraint.hasBag(bag));
+		
+		assertFalse(constraint.hasBag(new Bag('z', 80)));
+	}
+
+	@Test
+	public void testFromString() {
+		CapacityConstraint constraint = CapacityConstraint.fromString("1 3", bag, items);
+
+		assertEquals(1, constraint.minimum);
+		assertEquals(3, constraint.maximum);
+
+		// test for incorrect parameter quantity Exceptions
+		try {
+			constraint = CapacityConstraint.fromString("", bag, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The given String must contain exactly two parameters."));
+		}
+
+		try {
+			constraint = CapacityConstraint.fromString("1", bag, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The given String must contain exactly two parameters."));
+		}
+
+		try {
+			constraint = CapacityConstraint.fromString("1 2 3", bag, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The given String must contain exactly two parameters."));
+		}
+
+		// test for empty first param
+		try {
+			constraint = CapacityConstraint.fromString(" 1", bag, items);
+			fail("No exception thrown.");
+		}
+		catch (RuntimeException e) {
+			assertTrue(e.getMessage().equals("The first parameter contained in the given String must be at least one character long."));
+		}
 	}
 }
